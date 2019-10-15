2a14c36070d0dc7907e3c993e6d6b9ed59873ad6
FranciscoCustodio1995 <54550742+FranciscoCustodio1995@users.noreply.github.com>
2019-10-14 19:52:58 -0600

    Update LM35COD1.ino

--- LM35COD1.ino
+++ LM35COD1.ino
@@ -1,5 +1,5 @@
-int led=9;
-int temp;
+int led=9; 
+int temp; //declaramos nuestra variable de temperatura 
 int temC;
 boolean status=LOW;
 void setup()
@@ -9,14 +9,14 @@ void setup()
 }
 void loop()
 {
-  temp = analogRead(A0);
+  temp = analogRead(A0); //aca indicamos que temperatura viene de A0 el cual es el que resive la info
   //temp = (temp *0.634328358);
-  temp =(temp/1024.0)*500;
+  temp =(temp/1024.0)*500; //aca hacemos la conversion de la informacion a numeros 
   
 //temp = (5.0 * temp * 100.0)/1024.0;
-  Serial.write(temp);
+  Serial.write(temp); //esto nos permite que processing lo pueda leer
   delay(1000);
-  if(Serial.available()>0)
+  if(Serial.available()>0) //aca indicamos que el valor tiene que ser mayor que 0
   {
     byte dato = Serial.read();
     if(dato==65)
